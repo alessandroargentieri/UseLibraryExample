4fcbe3624d7fc3d09668c09f3827a4b1a76ebcf0
alessandroargentieri <alessandroargentieri@libero.it>
2016-08-30 20:23:54 +0200

    Inserita Activity con intent filter custom e richiamata da un'altra

--- app/src/main/java/argentieri/alessandro/uselibraryexample/MainActivity.java
+++ app/src/main/java/argentieri/alessandro/uselibraryexample/MainActivity.java
@@ -1,14 +1,16 @@
 package argentieri.alessandro.uselibraryexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import argentieri.alessandro.mylibrary.LibraryClass;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    String CUSTOM_ACTION = "argentieri.alessandro.uselibraryexample.GET_NUMBER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +19,15 @@ public class MainActivity extends AppCompatActivity {
 
         LibraryClass libraryClass = new LibraryClass("Alessandro");
         Toast.makeText(this, libraryClass.getSaluti(), Toast.LENGTH_LONG).show();
+
     }
+
+    public void Try_To_Get_Number(View v){
+        Intent i = new Intent();
+        i.setAction(CUSTOM_ACTION);
+        startActivity(i);
+        //controllo i risultati in onActivityResult
+    }
+
+
 }
