336b753d0c754a2f2b3f4386e89922d3989300c7
Alessandro Argentieri <alessandro.argentieri@cetma.local>
2016-08-31 09:11:54 +0200

    Added LibraryActivity call onResult and normal way

--- app/src/main/java/argentieri/alessandro/uselibraryexample/MainActivity.java
+++ app/src/main/java/argentieri/alessandro/uselibraryexample/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import argentieri.alessandro.mylibrary.LibraryClass;
@@ -11,6 +12,7 @@ import argentieri.alessandro.mylibrary.LibraryClass;
 
 public class MainActivity extends AppCompatActivity {
     String CUSTOM_ACTION = "argentieri.alessandro.uselibraryexample.GET_NUMBER";
+    int GET_NUMBER_REQUEST = 3243; //custom REQUEST_CODE
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +28,29 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent();
         i.setAction(CUSTOM_ACTION);
         startActivity(i);
+
+    }
+
+    public void startActivity4Result(View v){
+        Intent intent = new Intent();
+        intent.setAction(CUSTOM_ACTION);  //implicit intent because we don't define which Activity will open but only the intent action
+        startActivityForResult(intent, GET_NUMBER_REQUEST);
         //controllo i risultati in onActivityResult
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == GET_NUMBER_REQUEST && resultCode == RESULT_OK) {
+            String number_back = (String) data.getExtras().get("Result_number");
+            TextView tx = (TextView) findViewById(R.id.textResult);
+            tx.setText(number_back);
+        }
+    }
+
+
+
+
 
 
 }
--- mylibrary/src/main/java/argentieri/alessandro/mylibrary/LibraryActivity.java
+++ mylibrary/src/main/java/argentieri/alessandro/mylibrary/LibraryActivity.java
@@ -1,11 +1,16 @@
 package argentieri.alessandro.mylibrary;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LibraryActivity extends AppCompatActivity {
+
+    int GET_NUMBER_REQUEST = 3243; //custom REQUEST_CODE
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,13 @@ public class LibraryActivity extends AppCompatActivity {
         EditText edt = (EditText)findViewById(R.id.edit);
         String result = edt.getText().toString();
 
+        if(getCallingActivity() !=null) {
+            Intent output = new Intent();
+            output.putExtra("Result_number", result);
+            setResult(RESULT_OK, output);
+            finish();
+        }else{
+            Toast.makeText(this, "Not called for Result", Toast.LENGTH_LONG).show();
+        }
     }
 }
